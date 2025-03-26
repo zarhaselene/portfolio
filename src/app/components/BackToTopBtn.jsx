148b4c51c44f4316d556c8f36c1eefb50919a5d6
Zarha <zarhaselene@gmail.com>
2025-03-26 14:01:48 +0100

    Adjust positioning of BackToTopBtn and ScrollIndicator components for improved layout on different screen sizes

--- src/app/components/BackToTopBtn.jsx
+++ src/app/components/BackToTopBtn.jsx
@@ -43,7 +43,7 @@ const BackToTopBtn = () => {
       {isVisible && (
         <button
           onClick={scrollToTop}
-          className="fixed bottom-8 right-8 p-3 bg-primary border border-secondary/20 rounded-full shadow-lg transition-all duration-300 focus:outline-none focus:ring-1 focus:ring-secondary group hover:border-secondary hover:animate-jello"
+          className="fixed bottom-20 right-3 md:bottom-8 md:right-8 p-3 bg-primary border border-secondary/20 rounded-full shadow-lg transition-all duration-300 focus:outline-none focus:ring-1 focus:ring-secondary group hover:border-secondary hover:animate-jello"
           aria-label="Back to top"
         >
           <HiArrowNarrowUp className="w-5 h-5 text-secondary group-hover:text-secondary transition-colors duration-300" />
--- src/app/components/ScrollIndicator.jsx
+++ src/app/components/ScrollIndicator.jsx
@@ -28,7 +28,7 @@ const ScrollIndicator = () => {
 
   return (
     <div
-      className="absolute bottom-0 left-1/2 -translate-x-1/2 text-sm flex flex-col items-center"
+      className="absolute bottom-16 md:bottom-0 left-1/2 -translate-x-1/2 text-sm flex flex-col items-center"
       style={{ opacity }}
     >
       <span className="w-1.5 h-1.5 bg-secondary rounded-full absolute -top-2 left-1/2 -translate-x-1/2 animate-moveAndFade"></span>
