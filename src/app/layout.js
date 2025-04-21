abb47425e23daf5344390181881787a1d7966d67
Zarha <zarhaselene@gmail.com>
2025-04-21 12:00:20 +0200

    Add Vercel Analytics integration and update dependencies

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./styles/globals.css";
 import { Poppins, Bebas_Neue } from "next/font/google";
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${bebasNeue.variable} ${poppins.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
