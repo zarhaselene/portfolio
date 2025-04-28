67de7823ce2b16df06fd1d645c1fde467f4624cc
Zarha <zarhaselene@gmail.com>
2025-04-28 20:25:09 +0200

    Add suppressHydrationWarning to RootLayout HTML tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${bebasNeue.variable} ${poppins.variable} antialiased`}>
         <Navbar />
         {children}
