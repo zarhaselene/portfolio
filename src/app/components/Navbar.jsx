c64838f13b5c79bb2066368c5beace209b51dc90
Zarha <zarhaselene@gmail.com>
2025-04-28 14:30:07 +0200

    Refactor Navbar component: reorganize imports and enhance link accessibility with focus ring

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { ThemeSelector } from "./ThemeController";
-
+import Socials from "./Socials";
 const Navbar = () => {
   const [theme, setTheme] = useState("dark");
 
@@ -20,13 +20,18 @@ const Navbar = () => {
   return (
     <div className="flex items-center w-full min-h-16">
       <div className="flex-1">
-        <a className="text-lg font-header hover:text-secondary min-w-[9rem] inline-block ">
+        <a
+          tabIndex={0}
+          className="text-lg font-header hover:text-secondary inline-block focus-ring"
+          href="/"
+        >
           Zarha Selene
         </a>
       </div>
       <div className="flex-none">
         <ThemeSelector />
       </div>
+      <Socials />
     </div>
   );
 };
